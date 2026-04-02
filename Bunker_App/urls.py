52d6a7b439fcca69d7b65d8856af9920631bdc4b
unknown <bebiashvili.dachi@gmail.com>
2026-04-02 15:14:32 +0400

    Authinficate&partys

---
+++ Bunker_App/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import register_view, login_view, logout_view, main_view, create_party_view, find_party_view, join_party_view
+
+urlpatterns = [
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('', main_view, name='main'),
+    path('create/', create_party_view, name='create_party'),
+    path('find/', find_party_view, name='find_party'),
+    path('join/', join_party_view, name='join_party'),
+]
---
+++ Bunker_App/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .utils import login_required_message
+
+#register, login, logout
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect('login')
+        
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(
+            request,
+            username=request.POST.get('username'),
+            password=request.POST.get('password')
+        )
+        if user:
+            login(request, user)
+            return redirect('main') 
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, 'login.html')  
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required_message
+def main_view(request):
+    return render(request, 'main_page.html')
+
+
+@login_required_message
+def join_party_view(request):
+    return render(request, 'join_party.html')
+
+
+@login_required_message
+def create_party_view(request):
+    return render(request, 'create_party.html')
+
+
+@login_required_message
+def find_party_view(request):
+    return render(request, 'find_party.html')
