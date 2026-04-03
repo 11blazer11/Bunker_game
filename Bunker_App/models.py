e08069d5cc4733fb68e941ba0b8594c94a5831f2
unknown <bebiashvili.dachi@gmail.com>
2026-04-03 21:12:48 +0400

    charachteristics&random

--- Bunker_App/models.py
+++ Bunker_App/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Lobby(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.PositiveIntegerField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='User')
--- Bunker_App/views.py
+++ Bunker_App/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
+from charachteristics import *
 from django.contrib import messages
 from .utils import login_required_message
+import random
 
 #register, login, logout
 def register_view(request):
